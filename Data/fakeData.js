467fbf98eeba652c5ccf800df60cabb0f73b08f6
marukrutik <krutik.maru.memey@gmail.com>
2024-01-18 10:10:09 +0530

    Data Added ✅

---
+++ Data/fakeData.js
@@ -0,0 +1,6 @@
+const data = {
+    students: [
+        {name: "Krutik Maru", UID: "21BIT032", email: "maru.krutik.16423@gmail.com"},
+        {name: "Yash Chaubey", UID: "21BIT009", email: "chaubeyyash333@gmail.com"},
+    ]
+}
